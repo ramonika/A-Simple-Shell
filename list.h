117d2e4a4a87bca6b69cf4be524e259d118477f8
MonikaRajan <monikabd766@gmail.com>
2026-05-26 15:26:14 +0530

    Remove author name from list.h
    Removed author name from the header comment.

--- list.h
+++ list.h
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : list.c
- Author      : mahendra pratap singh
+ Author      : 
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
